3e897111e77f9df7223578672a4a57010e0034c7
PunkN <79531121998@mail.ru>
2024-10-13 20:40:31 +0300

    Второе ДЗ по строкам и методам

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,58 @@
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void checkYear(int year) {
+        if (year > 1584) {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                System.out.println(year + " год — високосный год");
+            } else {
+                System.out.println(year + " год — невисокосный год");
+            }
+        } else {
+            System.out.println("В " + year + " году ещё не вёлся отсчёт високосного года");
+        }
+    }
+    public static void checkOSAndYear(int clientOS1, int deviceYear ) {
+        if (deviceYear < 2015 && clientOS1 == 0) {
+            if (clientOS1 == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else if(deviceYear >= 2015 && clientOS1 == 0)
+                System.out.println("Установите  версию приложения для iOS по ссылке");
+        } else if (deviceYear < 2015 && clientOS1 == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (deviceYear >= 2015 && clientOS1 == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void finalDeliveryTime(int deliveryDistance, int deliveryDays) {
+        if (deliveryDistance <= 100) {
+            if (deliveryDistance > 0 && deliveryDistance < 20) {
+                System.out.println("Потребуется дней: " + deliveryDays);
+            } else if (deliveryDistance <= 60) {
+                System.out.println("Потребуется дней: " + (deliveryDays + 1));
+            } else if (deliveryDistance <= 100) {
+                System.out.println("Потребуется дней: " + (deliveryDays + 2));
+            }
+        } else {
+            System.out.println("Доставка невозможна");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Задание №1");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        int currentYear = LocalDate.now().getYear();
+        checkYear(currentYear);
 
         System.out.println("Задание №2");
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        int clientOS1 = 1; // переменную года устройства взял из currentYear в задании №1, которое определило текущий год
+        checkOSAndYear(clientOS1, currentYear);
 
         System.out.println("Задание №3");
-        String fullName1 = "Иванов Семён Семёнович";
-        fullName1 = fullName1.replace('ё', 'е');
-        System.out.println("Данные Ф. И. О. сотрудника — " + fullName1);
+        int deliveryDistance = 91;
+        int deliveryDays = 1;
+        finalDeliveryTime(deliveryDistance, deliveryDays);
     }
 }
